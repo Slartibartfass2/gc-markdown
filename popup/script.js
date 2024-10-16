6368d0f8e0c3ca9b9876aefa10f40e61a8feac6b
Felix Schlegel <felix.schlegel@gmx.de>
2024-10-17 00:54:39 +0200

    feat: Added available placeholders to popup

--- popup/script.js
+++ popup/script.js
@@ -4,6 +4,29 @@ if (typeof browser === "undefined") {
     var browser = chrome;
 }
 
+const placeholders = [
+    "title",
+    "gcCode",
+    "difficulty",
+    "terrain",
+    "isSolved",
+    "isFound",
+    "finalCoordinates",
+];
+const placeholderDescription = document.createElement("span");
+for (let i = 0; i < placeholders.length; i++) {
+    const placeholder = placeholders[i];
+    const placeholderElement = document.createElement("strong");
+    placeholderElement.textContent = placeholder;
+    placeholderDescription.appendChild(placeholderElement);
+    if (i < placeholders.length - 1) {
+        placeholderDescription.appendChild(document.createTextNode(", "));
+    }
+}
+document
+    .getElementById("placeholder-description")
+    .appendChild(placeholderDescription);
+
 const textArea = document.getElementById("template-text");
 const button = document.getElementById("save-template-btn");
 
